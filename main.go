451290018174c9586e15c0ac0d1e05f861589cf2
abhiraj ranjan <abhirajranjan@outlook.com>
2023-08-08 19:42:03 +0530

    changed uri group

--- main.go
+++ main.go
@@ -10,27 +10,27 @@ func main() {
 	DB := memorydb.NewMemoryDB()
 
 	router := gin.Default()
-	router.POST("/player", func(ctx *gin.Context) {
+	router.POST("/players", func(ctx *gin.Context) {
 		CreateNewPlayer(ctx, DB)
 	})
 
-	router.PUT("/player/:id", func(ctx *gin.Context) {
+	router.PUT("/players/:id", func(ctx *gin.Context) {
 		UpdatePlayer(ctx, DB)
 	})
 
-	router.DELETE("/player/:id", func(ctx *gin.Context) {
+	router.DELETE("/players/:id", func(ctx *gin.Context) {
 		DeletePlayer(ctx, DB)
 	})
 
-	router.GET("/player", func(ctx *gin.Context) {
+	router.GET("/players", func(ctx *gin.Context) {
 		GetAllPlayerRankwise(ctx, DB)
 	})
 
-	router.GET("/player/rank/:val", func(ctx *gin.Context) {
+	router.GET("/players/rank/:val", func(ctx *gin.Context) {
 		GetPlayerByRank(ctx, DB)
 	})
 
-	router.GET("/player/random", func(ctx *gin.Context) {
+	router.GET("/players/random", func(ctx *gin.Context) {
 		GetRandomPlayer(ctx, DB)
 	})
 
